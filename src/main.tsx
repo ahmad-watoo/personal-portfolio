b392151fff21f86319142ebb874b31771d9e1db9
ahmad-watoo <pakmuhammadahmad8@gmail.com>
2026-05-16 01:27:04 +0500

    resolve import issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
 
+// @ts-ignore: Allow side-effect import of CSS module without type declarations
 import './styles/globals.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
